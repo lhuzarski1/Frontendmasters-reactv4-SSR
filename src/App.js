f7c147244543bb16d53ab0defc6799430b077595
lhuzarski1 <lukaszhuzarski1@gmail.com>
2022-08-10 09:38:18 +0200

    SSR

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 import { StrictMode, useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 
@@ -10,7 +9,6 @@ const App = () => {
   return (
     <StrictMode>
       <ThemeContext.Provider value={theme}>
-        <BrowserRouter>
           <header>
             <Link to="/">Adopt Me!</Link>
           </header>
@@ -18,10 +16,9 @@ const App = () => {
             <Route path="/details/:id" element={<Details />} />
             <Route path="/" element={<SearchParams />} />
           </Routes>
-        </BrowserRouter>
       </ThemeContext.Provider>
     </StrictMode>
   );
 };
 
-render(<App />, document.getElementById("root"));
+export default App
